06f1fe898f0386bd5f5b93a038ce643ec0728f11
John Halbert <johnhalbert@gmail.com>
2015-07-29 00:17:11 -0700

    cleaning up deleteconfirm.js

--- client/js/directives/deleteConfirm.js
+++ client/js/directives/deleteConfirm.js
@@ -1,18 +1,7 @@
 sweetenio.directive('deleteConfirm', function(){
 	
-	console.log('deleteConfirm loaded');
 
 	return{
-		// link: function(scope, element, attrs){
-		// 	scope.$on('confirm-delete', function(e, notsure){
-		// 		$('#deleteModal')
-		// 			.modal({
-		// 				onApprove: del
-		// 			})
-		// 			.modal('show')
-		// 		;
-		// 	})
-		// },
 		templateUrl: '/partials/modals/delete.html'
 	}
 
